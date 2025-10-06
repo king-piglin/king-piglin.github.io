f340834c0b4a9f0b52e28525381a8d319f8c6091
nattomaki10000 <nattomaki10000@gmail.com>
2025-10-06 20:56:08 +0900

    ?

---
+++ Assets/main/index.js
@@ -0,0 +1,7 @@
+// JavaScript
+    function toggleMenu() {
+      const menu = document.getElementById('menu');
+      const trigger = document.querySelector('.hamburger');
+      menu.classList.toggle('open');
+      trigger.classList.toggle('open');
+    }
---
+++ README.md
@@ -0,0 +1,1 @@
+# king-piglin.github.io
